8436a4203b3319160135be61c9d0af49e82b17d0
Nikita Lazarev <nikita.u.lazarev@gmail.com>
2020-12-26 09:40:13 -0500

    Dagger RPC:   - fixed cycle alignment in ccip_transmitter

--- dagger/sw/ase_samples/joint_ase_process.cc
+++ dagger/sw/ase_samples/joint_ase_process.cc
@@ -7,6 +7,7 @@
 // server can be launched as independent standalone processes.
 //
 
+#include <future>
 #include <iostream>
 #include <vector>
 #include <thread>
@@ -21,17 +22,33 @@
 
 
 #define FRPC_LOG_LEVEL_GLOBAL FRPC_LOG_LEVEL_INFO
-#define NUMBER_OF_THREADS 1
-#define NUM_OF_REQUESTS 8
+#define NUMBER_OF_THREADS 4
+#define NUM_OF_REQUESTS 20
 
-static int run_server();
+static int run_server(std::promise<bool>& init_pr, std::future<bool>& cmpl_ft);
 static int run_client();
 
 int main() {
-    std::thread server_thread = std::thread(&run_server);
+    std::promise<bool> init_pr;
+    std::future<bool> init_ft = init_pr.get_future();
+
+    std::promise<bool> cmpl_pr;
+    std::future<bool> cmpl_ft = cmpl_pr.get_future();
+
+    // Start server
+    std::thread server_thread = std::thread(&run_server, std::ref(init_pr), std::ref(cmpl_ft));
+
+    // Wait until server is set-up
+    init_ft.wait();
+
+    // Start client
     std::thread client_thread = std::thread(&run_client);
 
+    // Wait untill client thread is terminated
     client_thread.join();
+    cmpl_pr.set_value(true);
+
+    // Wait until server thread is terminated
     server_thread.join();
 
     return 0;
@@ -46,7 +63,7 @@ int main() {
 static frpc::RpcRetCode loopback(uint64_t data, NumericalResult* ret);
 static frpc::RpcRetCode add(uint64_t a, uint64_t b, NumericalResult* ret);
 
-static int run_server() {
+static int run_server(std::promise<bool>& init_pr, std::future<bool>& cmpl_ft) {
     frpc::RpcThreadedServer server(SERVER_NIC_ADDR, NUMBER_OF_THREADS);
 
     // Init
@@ -85,7 +102,16 @@ static int run_server() {
     }
 
     std::cout << "------- Server is running... -------" << std::endl;
-    sleep(40); // Run server for 40 sec
+
+    // Notify client thread
+    init_pr.set_value(true);
+
+    //
+    // Work for a while
+    //
+
+    // Wait untill client thread terminates
+    cmpl_ft.wait();
 
     res = server.stop_all_listening_threads();
     if (res != 0)
@@ -110,7 +136,7 @@ static int run_server() {
 // RPC function #0
 static frpc::RpcRetCode loopback(uint64_t data, NumericalResult* ret) {
     std::cout << "loopback is called with data= " << data << std::endl;
-    ret->data = data + 10;
+    ret->data = data + 1;
     return frpc::RpcRetCode::Success;
 }
 
@@ -146,24 +172,19 @@ static int client(frpc::RpcClient* rpc_client, size_t thread_id, size_t num_of_r
         int res = rpc_client->loopback(thread_id*10 + i);
         assert(res == 0);
 
-        usleep(100000);
-
-        //res = rpc_client->add(thread_id*10 + i, thread_id*10 + i + 10);
-        //assert(res == 0);
-
-        //usleep(100000);
+        usleep(200000);
     }
 
     // Wait a bit
-    sleep(30);
+    sleep(60);
 
     // Read completion queue
     size_t n_of_cq_entries = cq->get_number_of_completed_requests();
     std::cout << "Thread " << thread_id << ", CQ entries: " << n_of_cq_entries << std::endl;
     for (int i=0; i<n_of_cq_entries; ++i) {
-        std::cout << "Thread " << thread_id << ", RPC returned: " << 
-                         *reinterpret_cast<int64_t*>(
-                            reinterpret_cast<NumericalResult*>(cq->pop_response().argv)) << std::endl;
+        std::cout << "Thread " << thread_id << ", RPC returned: "
+                  << reinterpret_cast<NumericalResult*>(cq->pop_response().argv)->data
+                  << std::endl;
     }
 
     return 0;
